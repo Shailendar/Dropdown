9b5534ff547ed277c6facb7eb6074ea3d1bc59ad
Shailendar Kanchanapalli <shailendar@COSYN.IN>
2018-08-04 11:32:42 +0530

    Add project files.

---
+++ Dropdown/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dropdown.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Display(Name = "Name")]
+        public string StudentName { get; set; }
+        public Gender StudentGender { get; set; }
+    }
+    public enum Gender
+    {
+        Male,
+        Female 
+    }
+}
